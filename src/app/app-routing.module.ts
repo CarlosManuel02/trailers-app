e19726598bb41043a7d9efc7982c952af20f7aca
CarlosManuel02 <94581164+CarlosManuel02@users.noreply.github.com>
2023-04-05 16:35:34 -0700

    Estructura basica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'traillers',
+    loadChildren: () => import('./traillers/traillers.module').then(m => m.TraillersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
