383cc915c423e421d00ae85a96c34d89d8ab5f2d
CarlosManuel02 <94581164+CarlosManuel02@users.noreply.github.com>
2023-04-06 22:35:19 -0700

    traillers service

--- src/app/traillers/services/traillers.service.ts
+++ src/app/traillers/services/traillers.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Category} from "../interfaces/category.interface";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TraillersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   private _categories: Category[] = [
     {
@@ -32,9 +34,70 @@ export class TraillersService {
         }
       ]
     },
+    {
+      category: 'Comedy',
+      trailers: [
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'}
+      ]
+    },
+    {
+      category: 'Drama',
+      trailers: [
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'},
+        {link: 'https://www.youtube.com/embed/vhumOLNSSJY'}
+      ]
+    }
   ];
 
   get categories() {
     return [...this._categories];
   }
+
+  getTrailers(category: string) {
+  }
+
+  getAllTrailers() {
+
+  }
+
+  getTrailerById(category: string, id: string) {
+  }
+
+  addTrailer(category: string, trailer: string) {
+  }
+
+  deleteTrailer(category: string, id: string) {
+
+  }
+
+  updateTrailer(category: string, id: string, trailer: string) {
+
+  }
+
+  getCategories() {
+
+  }
+
+  addCategory(category: string) {
+
+  }
+
+  deleteCategory(category: string) {
+
+  }
+
+  updateCategory(category: string, newCategory: string) {
+
+  }
+
+
 }
