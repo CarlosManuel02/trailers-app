7921b0a897b231e427dea6d0c7153e3706e75bb9
CarlosManuel02 <94581164+CarlosManuel02@users.noreply.github.com>
2023-04-07 11:48:46 -0700

    listado

--- src/app/traillers/pages/listado/listado.component.ts
+++ src/app/traillers/pages/listado/listado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TraillersService} from "../../services/traillers.service";
 
 @Component({
@@ -7,14 +7,38 @@ import {TraillersService} from "../../services/traillers.service";
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent {
+  isModalOpen = false;
+  searchText: string = '';
+  foundTraillers: any[] = [];
 
   constructor(
     private ts: TraillersService
   ) {
+    this.foundTraillers
   }
 
   get traillers() {
     return this.ts.categories;
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  search(searchText: string) {
+    this.searchText = searchText;
+
+    this.traillers.map((category) => {
+      category.trailers?.map((trailer) => {
+        if (trailer.category.includes(searchText)) {
+          this.foundTraillers.push(trailer)
+        } else {
+          console.log(false)
+        }
+      })
+    })
+
+
   }
 
 }
